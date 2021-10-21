5ee55855dc001854e3691f5a0f974fe096fdca19
rahul8.ranjan <rahul8.ranjan@gmail.com>
2021-10-21 10:09:03 +0530

    updating the locust test.

--- locustfile.py
+++ locustfile.py
@@ -1,69 +1,31 @@
-from locust import HttpLocust, TaskSet, task
-import json
+from locust import HttpUser, task, between
+import time
 import sys
 import random
-constants={}
 
-constants['credentials'] = {'9945240311': "jiomoney@2", '8880613625': "jiomoney@2", '9944031431': "jiomoney@2", '7204660988': "Qktest@1", '9945240316': "jiomoney@2"}
+constants = {}
+get_coupon_parameters = [{"version": "v5", "start": 0, "end": 10}, {"version": "v5", "start": 0, "externalMerchantId": "100001000069383", "categoryId": 1, "end": 10, "lat": 19.6712179806, "lng": 73.2293543592}, {"version": "v5", "start": 0, "categoryId": 1, "end": 10, "lat": 19.6712179806, "lng": 73.2293543592}]
+get_coupon_client = [{'x-client-type':'Rjil_jiokart',"x-loginid" : "9945240311"}, {'x-client-type':'mops', "x-loginid" : "9945240311"}, {'x-client-type':'microsite', "x-loginid" : "9945240311"}]
 
-class UserBehavior(TaskSet):
-    def login_for_access_token(l):
-        response = l.client.post("/jm/auth/oauth/v2/token",
-                                 data = {'username' :"9945240311", 'password' : "jiomoney@2", 'grant_type':"password"},
-                                 headers={
-                                     'Authorization': "Basic bDd4eDNlODg3NDAzYjVlZDQwZTc4Y2E4ZWRlZjY1Yzg3NTg3OmM2NDU1NjhhOTI3NzQ1YTY5NmUwZTUyZTU4NzFiZTgz",
-                                     'Content-Type': "application/x-www-form-urlencoded"
-                                 },
-                                 name="login_for_access_token")
-        constants["access_token"] = json.loads(response.text)["access_token"]
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    def get_coupons(self):
+        response = self.client.get("http://10.144.108.127:9000/coupons/v1/coupons/",
+                                   params=random.choice(get_coupon_parameters),
+                                   headers=random.choice(get_coupon_client),
+                                   name="get_coupons")
         print(response)
 
-    def get_category(l):
-        response = l.client.get("/cr/v2/coupons/category",
-                                params={"client":"myjio","version":"v5"},
-                                headers={'Authorization': "Bearer {}".format(constants["access_token"]),
-                                         'X-API-Key': "l7xx3e887403b5ed40e78ca8edef65c87587",
-                                         },
-                                name="get_category")
-        print(response)
-
-    def get_favourites(l):
-        response = l.client.get("/cr/v2/coupons/favorites",
-                                params={"version":"v5"},
-                                headers={
-                                    'Authorization': "Bearer {}".format(constants["access_token"]),
-                                    'X-API-Key': "l7xx3e887403b5ed40e78ca8edef65c87587",
-                                },
-                                name="get_favourites")
-        print(response)
-
-    def get_coupons(l):
-        response = l.client.get("/cr/v2/coupons",
-                                params={"categoryId":"1","start":"0","end":"10","version":"v4","lat":"12.97159","lng":"77.6056679"},
-                                headers={
-                                    'Authorization': "Bearer {}".format(constants["access_token"]),
-                                    'X-API-Key': "l7xx3e887403b5ed40e78ca8edef65c87587",
-                                    'X-CLIENT-TYPE': "myjio"
-                                },
-                                name="get_coupons")
-        print(response)
-
-    def on_start(l):
-        """
-        on_start is called when a Locust start before,
-        any task is scheduled
-        """
-        l.login_for_access_token()
     @task(1)
-    def user_workflow(l):
+    def user_workflow(self):
         sys.stdout.flush()
-        l.get_category()
-        l.get_favourites()
-        l.get_coupons()
+        self.get_coupons()
 
-
-class WebsiteUser(HttpLocust):
-    min_wait = 1000
-    max_wait = 1000
-    task_set = UserBehavior
-    host = "https:localhost:8083"
+#
+# class WebsiteUser(HttpUser):
+#     min_wait = 1000
+#     max_wait = 1000
+#     tasks = [QuickstartUser]
+#     host = "https:localhost:8089"
