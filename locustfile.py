b860b62940ffe5ba46e417c60f1d1d72a2f14015
rahul8.ranjan <rahul8.ranjan@gmail.com>
2021-10-24 02:14:49 +0530

    cohort-sync_final

--- locustfile.py
+++ locustfile.py
@@ -23,7 +23,7 @@ base_url = baseurl_sit
 anti_forgery = 0
 
 
-class QuickstartUser(HttpUser):
+class WithSession(HttpUser):
     wait_time = between(1, 5)
 
     def cohort_cron(self):
@@ -82,9 +82,36 @@ class QuickstartUser(HttpUser):
         self.anti_forgery = str(response.headers['x-anti-forgery'])
         print("login_cms", response.text)
 
-    @task
+    @task(1)
     def user_workflow(self):
         sys.stdout.flush()
         self.cohort_sync()
         self.cohort_cron()
+        # self.get_coupons()
+
+
+class WithoutSession(HttpUser):
+    wait_time = between(1, 5)
+
+    def get_coupons(self):
+        get_coupon_parameters = [{"version": "v5", "start": 0, "end": 10},
+                                 {"version": "v5", "start": 0, "externalMerchantId": "100001000069383", "categoryId": 1,
+                                  "end": 10, "lat": 19.6712179806, "lng": 73.2293543592},
+                                 {"version": "v5", "start": 0, "categoryId": 1, "end": 10, "lat": 19.6712179806,
+                                  "lng": 73.2293543592}]
+
+        get_coupon_client = [
+            {'x-client-type': 'Rjil_jiokart', "x-loginid": "9945240311"},
+            {'x-client-type': 'mops', "x-loginid": "9945240311"},
+            {'x-client-type': 'microsite', "x-loginid": "9945240311"}]
+
+        response = self.client.get(base_url + "/coupons/v1/coupons/",
+                                   params=random.choice(get_coupon_parameters),
+                                   headers=random.choice(get_coupon_client),
+                                   name="get_coupons")
+        print("get_coupon_api", response)
+
+    @task(1)
+    def user_workflow(self):
+        sys.stdout.flush()
         self.get_coupons()
